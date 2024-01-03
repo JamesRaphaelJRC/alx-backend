294fe02cd07552529cd00e02cb52c17e6372acda
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-03 19:40:45 +0100

    task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+''' Defines a class - FIFOCache '''
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    ''' Implements a FIFO caching system '''
+    def put(self, key, item):
+        '''
+        Updates the cache_data dictionary (inherited from BaseCaching)
+        with new cache data
+        '''
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Creates an iterator using the keys in cache_data and uses
+                # next to retrieve the next item (the first item)
+                first_item_key = next(iter(self.cache_data))
+                del self.cache_data[first_item_key]
+                print("DISCARD: " + first_item_key)
+            self.cache_data.update({key: item})
+
+    def get(self, key):
+        ''' Retrieves the value/cache item from the cache_data dictionary
+            using a given key
+        '''
+        if key is not None and key in self.cache_data:
+            return self.cache_data.get(key)
+        return None
