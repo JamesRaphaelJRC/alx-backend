9fd2539412287c722f2a38cb2e1111f6a3f95146
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-04 04:53:36 +0100

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+''' Defines a class - MRUCache '''
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    ''' Implements MRU (Most Recently Used) Caching style '''
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict(self.cache_data)
+
+    def put(self, key, item):
+        '''
+        Updates the cache_data dictionary (inherited from BaseCaching)
+        following the MRU caching algorithm
+        '''
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    mru_item = self.cache_data.popitem()
+                    print('DISCARD: ' + mru_item[0])
+            self.cache_data[key] = item
+
+    def get(self, key):
+        ''' Retrieves the value/cache item from the cache_data dictionary
+            using a given key
+        '''
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            value = self.cache_data[key]
+        else:
+            value = None
+        return value
